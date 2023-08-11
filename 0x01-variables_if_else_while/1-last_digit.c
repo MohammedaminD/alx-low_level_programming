3718673f208ca0190a2f84f8804e230839dc7206
MohammedaminD <mehamedamind@gmail.com>
2023-08-11 23:16:01 +0300

    C program for categorizing last digit number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *Description: last digit
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+	int lastdgt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdgt = n % 10;
+	if (lastdgt > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, lastdgt);
+	}
+	else if (lastdgt == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, lastdgt);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdgt);
+	}
+	return (0);
+}

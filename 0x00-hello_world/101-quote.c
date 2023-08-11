74bf6b7c95a1043754741daeb9812d6a964f8934
MohammedaminD <mehamedamind@gmail.com>
2023-08-11 18:18:49 +0300

    c additional

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistdio.h>
+#include <unistd.h>
+/**
+  *main-function that prints the 7th task quote to the standard errorr
+  *
+  *return- 1 (success)
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

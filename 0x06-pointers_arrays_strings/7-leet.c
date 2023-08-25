6a9c8b7f5784c228ca08b2487dddbcb4a7c34b55
MohammedaminD <mehamedamind@gmail.com>
2023-08-25 19:15:31 +0300

    indeed an 3l33t code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,24 @@
 #include "main.h"
 /**
  *char *leet -a function that
- * @* : char value input variable
- * Return:pointer value
+ * @str : char value input variable
+ * Return:string pointer value
  */
-char *leet(char *)
+char *leet(char *str)
+{
+	int i, j;
+	char c[] = "aAeEoOtTlL";
+	char d[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+			}
+		}
+	}
+	return (str);
+}

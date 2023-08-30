59be272d9fe8218cda4a8ad231fd1897196f96e7
MohammedaminD <mehamedamind@gmail.com>
2023-08-30 22:55:25 +0300

    fixing some errros

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,13 +14,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y != 0)
-	{
-		n = x * _pow_recursion(x, y - 1);
-		return (n);
-	}
 	else if (y == 0)
 	{
 		return (1);
+	}
+	else
+	{
+		n = x * _pow_recursion(x, y - 1);
+		return (n);
 	}
 }

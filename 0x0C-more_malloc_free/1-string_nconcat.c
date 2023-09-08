417ae4ba1fb9c20f5854a35567d9cd1ff0e68d9a
MohammedaminD <mehamedamind@gmail.com>
2023-09-08 23:46:57 +0300

    concatenate deez n

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *string_nconcat -a function that concatenates two strings
+ *
+ * @s1 : char variable pointer
+ * @s2 : char variable pointer
+ * @n : integer input variable
+ * Return:pointer value for success, NULL for fail
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, len1, len2;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+	ptr = (char *)malloc(sizeof(char) * len1 + n + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0 ; i < len1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < n; i++, j++)
+	{
+		ptr[i] = s2[j];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}

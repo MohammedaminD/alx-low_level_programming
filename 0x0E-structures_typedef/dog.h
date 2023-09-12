c72ab1856fef1a31f7d6f2354f548518db79cb70
MohammedaminD <mehamedamind@gmail.com>
2023-09-12 21:27:03 +0300

    dog header file with the structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,9 @@
 #ifndef _HEADER_
 #define _HEADER_
-
 /**
- * struct dog - Dog attributes
- * @name: The name of the dog.
- * @age: The age of the dog.
+ * struct dog - Attributes of the dog
+ * @name: name of the dog.
+ * @age: age of the dog.
  * @owner: The owner of the dog.
  *
  * Description: The attributes of a dog.
@@ -15,4 +14,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+
 #endif

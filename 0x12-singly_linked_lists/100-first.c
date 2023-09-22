039f174e3d672acfd8a59e1bb0a6f9a43fd03505
MohammedaminD <mehamedamind@gmail.com>
2023-09-23 02:20:35 +0300

    the constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+  * before_main- function that is excuted before main function
+  * Return: void
+  */
+void before_main(void)
+{
+	/**
+	  * The constructor attribute enables a
+	  * function to be executed before the main function
+	  */
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

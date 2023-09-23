a8ee6d70634b3e15d8f8a1c5f6e8c5b10d1538ec
MohammedaminD <mehamedamind@gmail.com>
2023-09-23 15:48:35 +0300

    another try

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,5 @@
 section .data
-text db 'Hello, holberton %s!', 0
-name db 'Hello, Holberton',10, 0
+text db 'Hello, Holberton', 10, 0
 
 section .text
 global main
@@ -11,14 +10,11 @@ push rbp
 mov rbp, rsp ; set up stack frame
 sub rsp, 16 ; reserve space for two pointers
 
-; load address of name string into rdi
-; the address of text string is already hardcoded in the instruction
-mov rdi, name
+; load address of text string into rdi
+lea rdi, [text]
 
-; load address of text string into rsi
-mov rsi, text
 xor eax, eax ; set return value to 0
-call printf ; call printf with two arguments
+call printf ; call printf with one argument
 
 add rsp, 16 ; restore the stack pointer
 mov rsp, rbp ; restore the stack pointer to the original position

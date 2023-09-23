a6e1126e1e3961787d0baa1b5f563cb3a34e0fa3
MohammedaminD <mehamedamind@gmail.com>
2023-09-23 15:40:49 +0300

    assembly language baby

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,26 @@
+section .data
+text db 'Hello, holberton %s!', 0
+name db 'Hello, Holberton',10, 0
+
+section .text
+global main
+extern printf
+
+main:
+push rbp
+mov rbp, rsp ; set up stack frame
+sub rsp, 16 ; reserve space for two pointers
+
+; load address of name string into rdi
+; the address of text string is already hardcoded in the instruction
+mov rdi, name 
+
+; load address of text string into rsi
+mov rsi, text
+xor eax, eax ; set return value to 0
+call printf ; call printf with two arguments
+
+add rsp, 16 ; restore the stack pointer
+mov rsp, rbp ; restore the stack pointer to the original position
+pop rbp ; restore the base pointer
+ret

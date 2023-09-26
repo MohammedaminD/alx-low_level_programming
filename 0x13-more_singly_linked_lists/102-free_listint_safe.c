651a784922957529aada165b2bcf19a28639473e
MohammedaminD <mehamedamind@gmail.com>
2023-09-26 22:52:40 +0300

    here goes ntn

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list(safe version)
+ * @head: double pointer to the head node
+ * Return: size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	listint_t *temp, *current;
+	size_t count = 0;
+
+	if (head == NULL)
+		return (0);
+
+	current = *head;
+	while (current != NULL)
+	{
+		count++;
+		temp = current;
+		current = current->next;
+		free(temp);
+		if (temp <= current)
+			break;
+	}
+	*head = NULL;
+	return (count);
+}

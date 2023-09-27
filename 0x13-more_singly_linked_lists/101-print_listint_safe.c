88da64fce527f6cf42b011cc51817c105a98c5d9
MohammedaminD <mehamedamind@gmail.com>
2023-09-27 23:42:01 +0300

    function that prints a single linked list elements

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - function that prints a singly linked list
+ * @head: pointer to the head of the linked list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *current = head, *temp;
+
+	while (current != NULL)
+	{
+		count++;
+		printf("[%p] %d\n", (void *)current, current->n);
+		temp = current;
+		current = current->next;
+		if (temp <= current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			exit(98);
+		}
+	}
+	return (count);
+}
